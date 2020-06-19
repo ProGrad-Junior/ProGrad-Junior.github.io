4f829aeb0bf6c061730182f673e6c8af0cb35b68
FACEPrep-ProGrad <61002120+FACEPrep-ProGrad@users.noreply.github.com>
2020-06-19 22:09:45 +0530

    Delete app.js

--- himanshahuja/app.js
+++
@@ -1,8 +0,0 @@
-function Greetings(){
-    var front = document.getElementById("front");
-    console.log(front);
-    front.style.visibility="hidden";
-    var back = document.getElementById("back");
-    back.style.visibility = "visible";
-  }
-  
